398921ab3166aa2c403bff261933685ef5dcda3a
Stacey Null <njyecats@yahoo.com>
2022-11-05 16:45:43 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// import './index.css';
+// import App from './App';
+// import  { Card, Button } from "react-bootstrap";
+// import MovieList from './components/movie-list';
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <App />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
